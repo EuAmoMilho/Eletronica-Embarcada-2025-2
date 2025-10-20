bff52323be88af020ebbbfc6aaeeb7b4e3a27371
Noboru <163944578+EuAmoMilho@users.noreply.github.com>
2025-10-20 10:16:48 -0300

    Add TP7 parte 2

---
+++ TP7parte2.X/main.c
@@ -0,0 +1,91 @@
+/**
+  Generated Main Source File
+
+  Company:
+    Microchip Technology Inc.
+
+  File Name:
+    main.c
+
+  Summary:
+    This is the main file generated using PIC10 / PIC12 / PIC16 / PIC18 MCUs
+
+  Description:
+    This header file provides implementations for driver APIs for all modules selected in the GUI.
+    Generation Information :
+        Product Revision  :  PIC10 / PIC12 / PIC16 / PIC18 MCUs - 1.81.8
+        Device            :  PIC16F1827
+        Driver Version    :  2.00
+*/
+
+/*
+    (c) 2018 Microchip Technology Inc. and its subsidiaries. 
+    
+    Subject to your compliance with these terms, you may use Microchip software and any 
+    derivatives exclusively with Microchip products. It is your responsibility to comply with third party 
+    license terms applicable to your use of third party software (including open source software) that 
+    may accompany Microchip software.
+    
+    THIS SOFTWARE IS SUPPLIED BY MICROCHIP "AS IS". NO WARRANTIES, WHETHER 
+    EXPRESS, IMPLIED OR STATUTORY, APPLY TO THIS SOFTWARE, INCLUDING ANY 
+    IMPLIED WARRANTIES OF NON-INFRINGEMENT, MERCHANTABILITY, AND FITNESS 
+    FOR A PARTICULAR PURPOSE.
+    
+    IN NO EVENT WILL MICROCHIP BE LIABLE FOR ANY INDIRECT, SPECIAL, PUNITIVE, 
+    INCIDENTAL OR CONSEQUENTIAL LOSS, DAMAGE, COST OR EXPENSE OF ANY KIND 
+    WHATSOEVER RELATED TO THE SOFTWARE, HOWEVER CAUSED, EVEN IF MICROCHIP 
+    HAS BEEN ADVISED OF THE POSSIBILITY OR THE DAMAGES ARE FORESEEABLE. TO 
+    THE FULLEST EXTENT ALLOWED BY LAW, MICROCHIP'S TOTAL LIABILITY ON ALL 
+    CLAIMS IN ANY WAY RELATED TO THIS SOFTWARE WILL NOT EXCEED THE AMOUNT 
+    OF FEES, IF ANY, THAT YOU HAVE PAID DIRECTLY TO MICROCHIP FOR THIS 
+    SOFTWARE.
+*/
+
+#include "mcc_generated_files/mcc.h"
+
+uint16_t duty = 0;
+
+
+/*
+                         Main application
+ */
+int main() {
+    // initialize the device
+    SYSTEM_Initialize();
+    
+    duty = ADC_GetConversion(channel_AN1);
+    
+    if(duty >= 0 && duty < 1023) {
+        EPWM2_LoadDutyValue(duty);
+    }
+    
+    // Enable the Global Interrupts
+    INTERRUPT_GlobalInterruptEnable();
+
+    // Enable the Peripheral Interrupts
+    INTERRUPT_PeripheralInterruptEnable();
+
+    while (1)
+    {
+        INTERRUPT_GlobalInterruptDisable(); // desativando interrupcoes
+                                            // para favorecer leitura correta
+                                            // do ADC e escrita PWM
+        duty = ADC_GetConversion(channel_AN1);
+        if(duty >= 0 && duty < 1023) {
+            EPWM2_LoadDutyValue(duty);
+        }    
+        INTERRUPT_GlobalInterruptEnable();
+        
+        asm("NOP"); // ciclos de trabalho para
+        asm("NOP"); // favorecer leitura correta
+        asm("NOP"); // da velocidade
+        asm("NOP");
+        
+        float spd = GetSpeedValue();        
+    }
+    
+    return 0;
+}
+/**
+ End of File
+*/
